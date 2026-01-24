543c452b4a67b93cc048410026c1d23a788cac3e
Liather <rory.cormack@outlook.com>
2026-01-24 14:19:36 +0000

    test

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,11 @@
 from flask import Flask
 from flask_socketio import SocketIO
-#from app.globals import findGames
+from app.globals import findGames
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
-#findGames()
+findGames()
 
 from app import routes, sockets
