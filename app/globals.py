f3fbaf54206df6fd504f4c12e0c1c5d688ec904c
Liather <rory.cormack@outlook.com>
2026-01-24 21:52:33 +0000

    Final commit tonight

--- app/globals.py
+++ app/globals.py
@@ -1,6 +1,12 @@
 from app.roomManager import roomManager
 
-roomManager = roomManager()
+roomManager = roogmManager()
 
 def findGames():
+    # look through the games folder
+    # iterate through each sub directory (each game)
+    # check for config.json
+        # if threre, read the expected files section
+        # check expected files are present
+        # if all present, add to list of available games
     print("test")
